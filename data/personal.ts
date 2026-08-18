3e026ba4fc1250425fb22002f2b08f03b43b4bf4
Vedant Salunkhe <143515542+Vedant-1107@users.noreply.github.com>
2026-08-18 20:09:13 +0530

    Update personal.ts

--- data/personal.ts
+++ data/personal.ts
@@ -11,7 +11,7 @@ export const personal = {
   github:
     "https://github.com/Vedant-1107",
   linkedin:
-    "https://www.linkedin.com/in/YOUR_USERNAME",
+    "www.linkedin.com/in/vedant-salunkhe-6961b5305",
   resume:
     "/resume.pdf",
   status: "Open to opportunities",
